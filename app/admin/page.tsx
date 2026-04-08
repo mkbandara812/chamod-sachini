a622f1a27bdd4104e0af16e47806e9fc0db7b5e9
mkbandara812 <111906483+mkbandara812@users.noreply.github.com>
2026-04-08 15:37:16 +0530

    Update page.tsx

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,7 +2,7 @@ import AdminPanel from "@/components/AdminPanel";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Admin Panel | Chamod & Sachini Wedding",
+  title: "Admin Panel | Dilshan & Sachini Wedding",
   description: "RSVP Admin Dashboard",
   robots: "noindex, nofollow",
 };
